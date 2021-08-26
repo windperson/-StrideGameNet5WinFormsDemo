c2fad3cef3ca4a0e04272128f2207fe2bbe418a1
Chen-Yu Pao <windperson@gmail.com>
2021-08-26 16:39:52 +0800

    Add project files.

---
+++ DemoNet5StrideGameWinform/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace DemoNet5StrideGameWinform
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(188, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(522, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Stride Game Engine .NET 5 Winform Demo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1043, 841);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Stride Engine Demo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DemoNet5StrideGameWinform/Form1.cs
@@ -0,0 +1,39 @@
+﻿using DemoStrideGame;
+using Stride.Engine;
+using Stride.Games;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoNet5StrideGameWinform
+{
+    public partial class Form1 : Form
+    {
+        private Game _game;
+        private GameContext _gameContext;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _gameContext = new GameContextWinforms(this);
+            _game = new TeapotDemo();
+            _game.WindowCreated += (sender, args) => {
+                if (sender is Game game)
+                {
+                    game.Window.AllowUserResizing = true;
+                }
+            };
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _game.Run(_gameContext);
+        }
+    }
+}
